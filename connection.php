bc719f0db657030990d4d37d653eabec62e3e567
Jefferson Gomes <jeffsilvaygo@gmail.com>
2019-06-27 23:11:05 -0300

    singleton class

---
+++ connection.php
@@ -0,0 +1,31 @@
+<?php
+	class Connection{
+		private static $instance = null;
+		private $conn;
+
+		private function __construct(){
+			$server = "localhost";
+			$user = "root";
+			$password = "";
+			$db = "usuarios";
+
+			try{
+				$this->conn = new PDO("mysql:host=$server;dbname=$db",$user,$password);
+
+				// set the PDO error mode to exception
+    			$this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+			}
+
+			catch(PDOException $e){
+				echo "Connection failed: ". $e->getMessage();
+			}
+		}
+
+		public static function getInstance(){
+			if(!isset(self::$instance)){
+				self::$instance = new Connection();
+			}
+			return self::$instance->conn;
+		}
+	}
+?>
