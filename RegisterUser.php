2093f492be2369e856ec9877255425acd6923911
Jefferson Gomes <jeffsilvaygo@gmail.com>
2019-06-28 13:33:26 -0300

    valid if already exists email registered

--- RegisterUser.php
+++ RegisterUser.php
@@ -2,21 +2,46 @@
 	require_once 'connection.php';
 	
 	class RegisterUser{
+		private $name, $lastName, $age, $email;
 		
 		public function __construct ($dados){
-			$name = $dados['nome'];
-			$surname = $dados['sobrenome'];
-			$age = $dados['idade'];
+			$this->name = $dados['name'];
+			$this->lastName = $dados['lastname'];
+			$this->age = $dados['age'];
+			$this->email = $dados['email'];
 
+			$this->checkUser();
+		}
+
+		private function checkUser(){
+			$pdo = Connection::getInstance();
+
+			$sql = "SELECT email FROM usuarios WHERE email = '$this->email' LIMIT 1";
+
+			$select = $pdo->query($sql);
+
+			$result = $select->fetch(PDO::FETCH_OBJ); // object returns
+
+			if(!isset($result->email)){
+				$this->insertUser();		
+			}
+
+			else{
+				echo "There is already a registration with this email.";
+			}
+
+		}
+
+		private function insertUser(){
 			try{			
 			
 				$pdo = Connection::getInstance();
 
-				$sql = "INSERT INTO usuarios (nome, sobrenome, idade) VALUES (?,?,?)";
+				$sql = "INSERT INTO usuarios (nome, sobrenome, idade, email) VALUES (?,?,?,?)";
 
 				$stmt= $pdo->prepare($sql);
 
-				$stmt->execute([$name, $surname, $age]);
+				$stmt->execute([$this->name, $this->lastName, $this->age, $this->email]);
 
 				echo "User successfully registered";				
 			}
@@ -25,7 +50,7 @@
 				echo $sql . "<br>" . $e->getMessage();
 			}
 
-			$stmt = null;
+			$pdo = null;			
 		}
 	}
 
